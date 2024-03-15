39186105deb9d9342a9bb87660fe67b438545728
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2024-03-15 21:05:31 +0000

    completes reqs

--- app.rb
+++ app.rb
@@ -37,3 +37,15 @@ end
 get("/random/results") do
   erb(:random_results)
 end
+
+def calculate_payments()
+  apr = params[:user_apr].to_f / 100
+  years = params[:user_years].to_f
+  present_value = params[:user_pv].to_f
+
+  monthly_interest_rate = apr / 12
+  num_payments = years * 12
+  payment = present_value * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-num_payments)))
+  payment.to_fs(:currency)
+  
+end
